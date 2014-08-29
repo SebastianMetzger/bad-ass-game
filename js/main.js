a7730b10162a156165c34e24aa66a705194e08bf
Sebastian <sebastian.metzger@gmx.net>
2014-08-30 00:20:41 +0200

    getting more reactive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-(function(){
+(function() {
 	var actors = [];
 
 	var hero = {
 		domElement: document.querySelector('#hero'),
 		x: 40,
-		y: 40
+		y: 40,
+		vx: 0,
+		vy: 0
 	};
 	actors.push(hero);
 
-	function generateVillain(){
+	function generateVillain() {
 		var element = document.createElement('div');
 		element.className = 'villain';
 		element.innerText = '>:(';
@@ -16,12 +18,14 @@
 		return {
 			domElement: element,
 			x: getRandomInt(0, 800),
-			y: getRandomInt(0, 600)
+			y: getRandomInt(0, 600),
+			vx: 0,
+			vy: 0
 		};
 	}
 
 	var villains = [];
-	for(var i = 0; i < 10; i++){
+	for (var i = 0; i < 10; i++) {
 		var villain = generateVillain();
 		actors.push(villain);
 		villains.push(villain);
@@ -31,43 +35,46 @@
 	var stream = Rx.Observable.interval(100);
 
 	//handle keyboard input
-	var input = Rx.Observable.fromEvent(document, 'keydown');
+	var keyDowns = Rx.Observable.fromEvent(document, 'keydown');
+	var keyUps = Rx.Observable.fromEvent(document, 'keyup');
 
-	input.subscribe(function(x){
-		var kc = x.keyCode;
+	keyDowns.merge(keyUps).select(function(x) {
+		return x.keyCode;
+	}).distinctUntilChanged().subscribe(function(kc) {
+		console.log(kc);
 		//left
-		if(kc === 37){
-			hero.x = Math.max(hero.x - 10,0);
-		//up
-		} 
-		if(kc === 38){
-			hero.y = Math.max(hero.y - 10,0);
-		//right
-		} 
-		if(kc === 39){
-			hero.x = Math.min(hero.x + 10,document.body.clientWidth);
-		//down
-		} 
-		if(kc === 40){
-			hero.y = Math.min(hero.y + 10,document.body.clientHeight);
+		if (kc === 37) {
+			hero.vx = Math.max(hero.x - 10, 0);
+			//up
+		}
+		if (kc === 38) {
+			hero.vy = Math.max(hero.y - 10, 0);
+			//right
+		}
+		if (kc === 39) {
+			hero.vx = Math.min(hero.x + 10, document.body.clientWidth);
+			//down
+		}
+		if (kc === 40) {
+			hero.vy = Math.min(hero.y + 10, document.body.clientHeight);
 		}
 		render();
 	});
 
-	function handleVillainMovement(){
-		villains.forEach(function(villain){
-			var direction = getRandomInt(0,3);
-			if(direction === 0){
-			villain.x = Math.max(villain.x - 10,0);
-			//up
-			} else if(direction === 1){
-				villain.y = Math.max(villain.y - 10,0);
-			//right
-			} else if(direction === 2){
-				villain.x = Math.min(villain.x + 10,document.body.clientWidth);
-			//down
-			} else if(direction === 3){
-				villain.y = Math.min(villain.y + 10,document.body.clientHeight);
+	function handleVillainMovement() {
+		villains.forEach(function(villain) {
+			var direction = getRandomInt(0, 3);
+			if (direction === 0) {
+				villain.vx = -10;
+				//up
+			} else if (direction === 1) {
+				villain.vy = -10;
+				//right
+			} else if (direction === 2) {
+				villain.vx = 10
+				//down
+			} else if (direction === 3) {
+				villain.vy = 10;
 			}
 		});
 		render();
@@ -75,10 +82,10 @@
 
 	stream.subscribe(handleVillainMovement);
 
-	function render(){
-		actors.forEach(function(actor){
-			actor.domElement.style.left = actor.x + "px";
-			actor.domElement.style.top = actor.y + "px";
+	function render() {
+		actors.forEach(function(actor) {
+			actor.domElement.style.left = (actor.x + actor.vx) + "px";
+			actor.domElement.style.top = (actor.y + actor.yx) + "px";
 		});
 	}
 
@@ -86,6 +93,12 @@
 
 	//Util
 	function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function always(value) {
+		return function() {
+			return value;
+		};
 	}
 })();
